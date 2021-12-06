ba65a32f12c947143c25a68a6594de884e14ca51
Peter Chen <peter_chen@trimble.com>
2021-12-06 10:57:49 -0700

    Add concurrency exception extensions and add delete concurrently case

--- EFCorePractice.Tests/ConcurrenctyTests.cs
+++ EFCorePractice.Tests/ConcurrenctyTests.cs
@@ -12,12 +12,12 @@ namespace EFCorePractice.Tests
     public class ConcurrenctyTests : TestBase
     {
         public ConcurrenctyTests(ITestOutputHelper output, DbContextFixture fixture)
-            :base(output, fixture)
+            : base(output, fixture)
         {
         }
 
         [Fact]
-        public async Task Concurrencty_Update_ThrowException()
+        public async Task Concurrency_Update_ThrowException()
         {
             // Arrange
             var context_a = await dbFixture.CreateContextAsync();
@@ -26,7 +26,7 @@ namespace EFCorePractice.Tests
             {
                 FirstName = "William",
                 LastName = "Shakespeare",
-                Email = "a_b@c.com"
+                Email = "a_b_" + DbContextFixture.NewUniqueIndex + "@c.com"
             };
             context_a.Add(contact_a);
             await context_a.SaveChangesAsync();
@@ -49,6 +49,34 @@ namespace EFCorePractice.Tests
             // Assert
             context_a.Entry(contact_a).Reload();
             Assert.Equal(contact_b.FirstName, contact_a.FirstName);
+        }
+
+        [Fact]
+        public async Task Concurrency_Delete_ThrowException()
+        {
+            // Arrange
+            var context_a = await dbFixture.CreateContextAsync();
+            var context_b = await dbFixture.CreateContextAsync();
+            var contact = new Contact
+            {
+                FirstName = "William",
+                LastName = "Shakespeare",
+                Email = "a_b_" + DbContextFixture.NewUniqueIndex + "@c.com"
+            };
+            var entity = context_a.Add(contact);
+            await context_a.SaveChangesAsync();
+            entity.State = EntityState.Detached;
+
+            // Act
+            var ca = await context_a.Contacts.SingleAsync(c => c.Email == contact.Email);
+            var cb = await context_b.Contacts.SingleAsync(cb => cb.Email == contact.Email);
+            context_a.Remove(ca);
+            await context_a.SaveChangesAsync();
+            context_b.Remove(cb);
+            
+            // Assert
+            await Assert.ThrowsAsync<DbUpdateConcurrencyException>(() => context_b.SaveChangesAsync());
+            await context_b.SaveChangesAsync_DatabaseWins();
         }
     }
 }
---
+++ EFCorePractice/DbContextConcurrencyExtension.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCorePractice
+{
+    public static class DbContextConcurrencyExtension
+    {
+        /// <summary>
+        /// "Database Win" strategy.
+        /// </summary>
+        public static async Task SaveChangesAsync_DatabaseWins(this DbContext ctx)
+        {
+            bool saveFailed;
+            do
+            {
+                saveFailed = false;
+
+                try
+                {
+                    await ctx.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    saveFailed = true;
+                    // Reload the values of the entities that failed to save from the db 
+                    foreach (var entry in ex.Entries)
+                        await entry.ReloadAsync();
+                }
+
+            } while (saveFailed);
+        }
+
+        /// <summary>
+        /// "Client Win" strategy.
+        public static async Task SaveChangesAsync_ClientWins(this DbContext ctx)
+        {
+            bool saveFailed;
+            do
+            {
+                saveFailed = false;
+
+                try
+                {
+                    await ctx.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    saveFailed = true;
+                    // Update original values from the database 
+                    foreach (var entry in ex.Entries)
+                    {
+                        var originalValues = await entry.GetDatabaseValuesAsync();
+                        entry.OriginalValues.SetValues(originalValues);
+                    }
+                }
+
+            } while (saveFailed);
+        }
+
+        /// <summary>
+        /// "Merge Client Modifies" strategy will reload database values and merge client modified values.
+        /// </summary>
+        public static async Task SaveChangesAsync_MergeClientModifies(this DbContext ctx)
+        {
+            bool saveFailed;
+            do
+            {
+                saveFailed = false;
+
+                try
+                {
+                    await ctx.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    saveFailed = true;
+                    // Merge values 
+                    foreach (var entry in ex.Entries)
+                    {
+                        var databaseValues = await entry.GetDatabaseValuesAsync();
+
+                        // Clone the values currently in the database an initial set of resolved values.
+                        var resolvedValues = databaseValues.Clone();
+                        // Select modified values from current values.
+                        var modifiedValues = entry.Properties.Where(p => p.IsModified).ToDictionary(p => p.Metadata.Name, p => p.CurrentValue);
+                        // Merge modified values to resolved values.
+                        resolvedValues.SetValues(modifiedValues);
+
+                        // Update the original values with the database values
+                        // Update the current values with resolved values.
+                        entry.OriginalValues.SetValues(databaseValues);
+                        entry.CurrentValues.SetValues(resolvedValues);
+                    }
+                }
+
+            } while (saveFailed);
+        }
+    }
+}
